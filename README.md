4f428dba88d6a664042c30d42ac592db99510554
Luis Crespo <lcrespo@es.ibm.com>
2019-05-15 17:16:10 +0200

    Converting md to html and replacing

---
+++ README.md
@@ -0,0 +1,5 @@
+# openmd
+A simple utility to display a markdown file in the browser.
+
+## Usage
+node openmmd [filename.md]
--- openmd.js
+++ openmd.js
@@ -1,4 +1,6 @@
 const fs = require('fs')
+const path = require('path')
+const marked = require('marked')
 
 function loadMD() {
 	if (process.argv.length < 3) {
@@ -7,7 +9,7 @@ function loadMD() {
 	}
 	let fname = process.argv[2]
 	try {
-		return [fname, fs.readFileSync(fname, 'utf-8')]
+		return [path.basename(fname), fs.readFileSync(fname, 'utf-8')]
 	}
 	catch (err) {
 		console.log('Could not load file ' + fname)
@@ -15,10 +17,19 @@ function loadMD() {
 	}
 }
 
+function replaceTag(text, tag, value) {
+	let result = []
+	for (let line of text.split('\n')) {
+		result.push(line.replace(`{{${tag}}}`, value))
+	}
+	return result.join('\n')
+}
+
 function main() {
 	let [fname, md] = loadMD()
 	let html = fs.readFileSync('index.html', 'utf-8')
-	html.replace('{{title}}', fname)
+	html = replaceTag(html, 'title', fname)
+	html = replaceTag(html, 'content', marked(md))
 	console.log(html)
 }
 
